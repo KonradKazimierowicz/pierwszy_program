0caf5cab2cd0b1697775ef5428307469a08ef848
konrad.kazimierowicz <vinseq7@gmail.com>
2023-03-09 10:31:16 +0100

    kotlin

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,7 +1,28 @@
 fun main(args: Array<String>) {
+
+
+
+    var zmInt = 1 //int
+    var zmLon = 1L //long
+    var zmInt2:Int = 10 //int
+    var zmLong:Long = 10 //long
+
+    var zmByte: Byte = 4 //byte
+    var zmShort: Short = 4 //short
+
+    var zmFloat = 10.2f //float
+    var zmDouble = 10.2 //double
+
+    var zmBool = true //bool
+
+    var zmChar = 'a' //char
+    var zmString = "Ala ma kota"
+
+
+    //Stale:
+    val stala = 10
+
+
     println("Hello World!")
 
-    // Try adding program arguments via Run/Debug configuration.
-    // Learn more about running applications: https://www.jetbrains.com/help/idea/running-applications.html.
-    println("Program arguments: ${args.joinToString()}")
 }
